04b13210ccbcaef5c67f2a85701cc2f201786f4f
zesettler <gromovgrin@gmail.com>
2023-07-04 19:16:27 +0300

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {findAllByDisplayValue} from "@testing-library/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +11,13 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+      <div>
+          <button>
+              <h1>
+
+              </h1>
+          </button>
+      </div>
   </React.StrictMode>
 );
 
@@ -17,3 +25,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//git
